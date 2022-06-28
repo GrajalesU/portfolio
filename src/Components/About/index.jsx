a395bd58687fa6da8e2d729826763cb6e04ed912
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-06-28 17:52:06 -0500

    setting links to other tab

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -20,9 +20,9 @@ const About = () => {
                     <p>codificando tus ideas</p>
                     <div className="social_media_icons">
                         <a href="https://github.com/GrajalesU">
-                            <img src="GitHub_mobile.png" alt="Github logo" />
+                            <img src="GitHub_mobile.png" alt="Github logo"  target="_blank" rel="noreferrer"/>
                         </a>
-                        <a href="https://www.linkedin.com/in/juan-manuel-grajales-urquijo-8aa78b225">
+                        <a href="https://www.linkedin.com/in/juan-manuel-grajales-urquijo-8aa78b225" target="_blank" rel="noreferrer">
                             <img src="LI_mobile.png" alt="LinkedIn logo" />
                         </a>
                         <a href="./cv_JuanManuelGrajales.pdf" target="_blank" >
